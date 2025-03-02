66c69cf2a11ca61f23e65c07e6612004e13ca09d
donydony228 <donydony228@gmail.com>
2025-03-02 13:51:49 +0800

    fix: GET/locations API connect

--- assets/js/api.js
+++ assets/js/api.js
@@ -27,44 +27,41 @@ async function fetchLocations() {
 
 
 
-// function addMarkersToMap(locations) {
-//     locations.forEach(loc => {
-//         const lat = parseFloat(loc.lat);
-//         const lon = parseFloat(loc.lon);
-
-//         if (!isNaN(lat) && !isNaN(lon)) {
-//             L.marker([lat, lon]).addTo(map)
-//                 .bindPopup(`<b>${loc.name}</b>`)
-//                 .openPopup();
-//         }
-//     });
-// }
-
 function addMarkersToMap(locations) {
     locations.forEach(loc => {
-        const lat = parseFloat(loc.lat);
-        const lon = parseFloat(loc.lon);
+        const lat = parseFloat(loc.latitude);
+        const lon = parseFloat(loc.longitude);
 
         if (!isNaN(lat) && !isNaN(lon)) {
-            // 自訂圖標
-            var customIcon = L.icon({
-                iconUrl: 'assets/images/pin.png', // 替換成你的圖標路徑
-                iconSize: [40, 40], // (寬, 高)
-                iconAnchor: [16, 40], // 讓圖標尖端指向座標
-                popupAnchor: [3, -40] // 彈出視窗位置
-            });
-
-            // 建立帶有自訂圖標的標記
-            L.marker([lat, lon], { icon: customIcon }).addTo(map)
-            .bindPopup(`
-                <div style="text-align: center;">
-                    <h4 style="margin: 1px 0; color: #333;">${loc.name}</h4>
-                    <p style="font-size: 12px; color: gray;">${loc.description || "我們的基地"}</p>
-                </div>
-            `).openPopup();
+            L.marker([lat, lon]).addTo(map)
+                .bindPopup(`<b>${loc.name}</b>`)
+                .openPopup();
         }
     });
 }
+
+// function addMarkersToMap(locations) {
+//     locations.forEach(loc => {
+//         const lat = parseFloat(loc.latitude);
+//         const lon = parseFloat(loc.longitude);
+
+//         if (!isNaN(lat) && !isNaN(lon)) {
+//             // 自訂圖標
+//             var customIcon = L.icon({
+//                 iconUrl: 'assets/images/pin.png', // 替換成你的圖標路徑
+//                 iconSize: [40, 40], // (寬, 高)
+//                 iconAnchor: [16, 40], // 讓圖標尖端指向座標
+//                 popupAnchor: [3, -40] // 彈出視窗位置
+//             });
+
+//             // 建立帶有自訂圖標的標記
+//             L.marker([lat, lon], { icon: customIcon }).addTo(map)
+//                 .on('click', function () {
+//                     updateSidebar(loc);
+//                 });
+//         }
+//     });
+// }
 
 
 document.addEventListener("DOMContentLoaded", fetchLocations);
